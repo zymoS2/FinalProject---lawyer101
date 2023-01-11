0470ed49c36fa8fbbe1ce3587f34bf09bebb6ea7
wjdghks95 <wjdghks2799@Naver.com>
2023-01-11 16:58:57 +0900

    first commit

---
+++ src/main/java/com/kh/lawservice101/Lawservice101Application.java
@@ -0,0 +1,13 @@
+package com.kh.lawservice101;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lawservice101Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Lawservice101Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/kh/lawservice101/Lawservice101ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kh.lawservice101;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lawservice101ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
